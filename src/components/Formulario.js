7491df1cd23b334e6dda2ecfaee600d8ad40bd83
Eduardo Poblete <eduardopoblete1998@gmail.com>
2021-03-09 14:46:13 -0300

    Primer commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+import useMoneda from '../hooks/useMoneda';
+import Error from './Error';
+import useCriptomoneda from '../hooks/useCriptomoneda';
+import axios from 'axios';
+
+const Boton = styled.input`
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    background-color: #66A2FE;
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    color: #FFF;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #326AC0;
+        cursor:pointer;
+    }
+`;
+
+const Formulario = ({guardarMoneda, guardarCriptomoneda}) => {
+
+    //State del listado de criptomonedas
+
+    const [listacripto, guardarCriptomonedas] = useState([]);
+    const [error, guardarError] = useState(false);
+
+    const MONEDAS = [
+        { codigo: 'USD', nombre:'Dolar de Estados Unidos'},
+        { codigo: 'CLP', nombre:'Pesos Chilenos'},
+        { codigo: 'MXN', nombre:'Pesos Mexicanos'},
+        { codigo: 'EUR', nombre:'Euro'}
+    ]
+
+    //Utilizar useMoneda 
+    const [moneda, SelectMonedas] = useMoneda('Selecciona tu moneda ', '', MONEDAS);
+    //Utilizar useCriptomoneda
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Selecciona tu criptomoneda ', '', listacripto);
+    //Ejecutar llamado a la API
+    useEffect(() => {
+        const consultarAPI = async() => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+            const resultado = await axios.get(url);
+
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI();
+    }, []);
+
+    //cuando el usuario hace submit
+    const cotizarMoneda = e => {
+        e.preventDefault();
+
+        //validar si ambos campos están llenos
+        if(moneda === '' || criptomoneda === ''){
+            guardarError(true);
+            return;
+        }
+        //pasar los datos al componente principal
+        guardarError(false);
+        guardarMoneda(moneda);
+        guardarCriptomoneda(criptomoneda);
+    }
+
+    return ( 
+        <form onSubmit={cotizarMoneda}>
+
+            { error ? <Error mensaje="Todos los campos son obligatorios" /> : null}
+
+            <SelectMonedas />
+
+            <SelectCripto />
+
+            <Boton 
+                type="submit"
+                value="Calcular"
+            />
+        </form>
+     );
+}
+ 
+export default Formulario;
+
